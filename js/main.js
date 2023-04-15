2a6f389971a041d72a979e091d06ffaae33ed224
koany <koroliev@yandex.ru>
2023-04-15 15:56:23 +0300

    Готово

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 let x = []
 
-// $(document).ready(function (){$('#result').text(x)})
 
+//добавить элемент
 function fAdd() {
     if (document.getElementById("in").checkValidity() && $('#in').val()) {
         x.push($('#in').val())
     }
-    $('#result').text(x)
+    printArray()
     $('#in').val('')
+    checkInput()
+    checkEmpty()
 }
 
+//удалить повторы
 function deleteRepeat(massive) {
     for (let i = 0; i < massive.length; i++) {
         for (let n = i + 1; n < massive.length; n++) {
@@ -21,20 +24,55 @@ function deleteRepeat(massive) {
     }
     // let temp = x.filter((element, index) => {
     // return x.indexOf(element) === index})
-    $('#result').text(x)
+    printArray()
+    checkEmpty()
 }
 
-$('#add1').click(fAdd)
+//проверка ввода числа в поле
+function checkInput() {
+    if (($('#in').val() != '') && document.getElementById("in").checkValidity()) {
+        $('#add1').prop('disabled', false)
+    } else {$('#add1').prop('disabled', true)}
+}
 
+//проверка массива на пустоту
+function checkEmpty() {
+    if (x.length) {
+        $('.deletes').prop('disabled', false)
+    } else {$('.deletes').prop('disabled', true)}
+}
+
+//отекстовка результата
+function printArray() {
+    if (x.length) {
+        $('#result').text('Ваш массив: [' + x + ']')
+    } else {$('#result').text('Ваш массив пуст. Добавьте значения')}
+}
+
+
+
+//удалить последний
 $('#delLast').click(function (){
     x.pop()
-    $('#result').text(x)
+    printArray()
+    checkEmpty()
 })
 
+//удалить первый
 $('#delFirst').click(function (){
     x.shift()
-    $('#result').text(x)
+    printArray()
+    checkEmpty()
 })
 
+
+$('#add1').click(fAdd)
 $('#delRepeat').click(function () {deleteRepeat(x)})
+$(document).ready(checkInput)
+$(document).ready($('#in').val(''))
+$('#in').keyup(checkInput)
+$('#in').change(checkInput)
+
+
+
 
